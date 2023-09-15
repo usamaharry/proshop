3d5519b62ae0334fd6ef867ffbd9e950016136bc
Usama Javed <usamajaved1119@icloud.com>
2023-09-15 21:26:18 +0500

    added rating

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,8 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import { Rating } from "./Rating";
+
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
@@ -14,6 +16,11 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`(${product.numReviews})`} />
+        </Card.Text>
+
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
