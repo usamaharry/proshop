793b25b844d8ed946a1134c20ef8c8650eba19f6
Usama Javed <usamajaved1119@icloud.com>
2023-09-16 15:09:56 +0500

    setup backend nad products api

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import products from "./data/products.js";
+
+dotenv.config();
+
+const port = process.env.PORT;
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("Api is running...");
+});
+
+app.get("/api/products", (req, res, next) => {
+  res.json(products);
+});
+
+app.get("/api/product/:id", (req, res, next) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`Server is running at ${port}`));
