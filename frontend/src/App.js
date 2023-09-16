6be7d71a13079b8df4c56d5bede73039cc8295e8
Usama Javed <usamajaved1119@icloud.com>
2023-09-16 14:42:10 +0500

    added product detail screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Header />
       <main>
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
     </>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,11 +6,13 @@ import {
 
 import App from "./App";
 import HomeScreen from "./screens/HomeScreen";
+import ProductDetailScreen from "./screens/ProductDetailScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductDetailScreen />} />
     </Route>
   )
 );
